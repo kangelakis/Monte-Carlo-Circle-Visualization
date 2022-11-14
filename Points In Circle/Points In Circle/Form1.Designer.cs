9a0d7973cef5a7aa9df90b518ad9e356f918c6ac
Konstantinos Angelakis <k.aggelakis8@gmail.com>
2022-11-14 20:03:59 +0200

    Add files via upload

---
+++ Points In Circle/Points In Circle/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Points_In_Circle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultsTextbox = new System.Windows.Forms.TextBox();
+            this.pointsTextbox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.resultsTextbox);
+            this.panel1.Controls.Add(this.pointsTextbox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 461);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(628, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(657, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Results";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(634, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Points";
+            // 
+            // resultsTextbox
+            // 
+            this.resultsTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsTextbox.Location = new System.Drawing.Point(628, 295);
+            this.resultsTextbox.Name = "resultsTextbox";
+            this.resultsTextbox.ReadOnly = true;
+            this.resultsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.resultsTextbox.TabIndex = 1;
+            // 
+            // pointsTextbox
+            // 
+            this.pointsTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pointsTextbox.Location = new System.Drawing.Point(628, 124);
+            this.pointsTextbox.Name = "pointsTextbox";
+            this.pointsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.pointsTextbox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox resultsTextbox;
+        private System.Windows.Forms.TextBox pointsTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Points In Circle/Points In Circle/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace Points_In_Circle
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> Points = new List<Point>();
+        private Point middle = new Point();
+        private int width = 300, height = 300; //Sizes of rectangle
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            middle.X = panel1.Width / 2;
+            middle.Y = panel1.Height / 2;
+            int x = middle.X - width / 2;
+            int y = middle.Y - height / 2;
+            int radius = (int)Math.Sqrt(Math.Pow(middle.X - x, 2));
+            float c = 0;
+
+            Rectangle rect = new Rectangle(x, y, width, height);
+
+            foreach (Point p in Points)
+            {
+                int distance = (int)Math.Sqrt(Math.Pow(middle.X - p.X, 2) + Math.Pow(middle.Y - p.Y, 2));
+                if (distance <= radius)
+                {
+                    e.Graphics.FillRectangle((Brush)Brushes.Lime, p.X, p.Y, 1, 1);
+                    c++;
+                }
+                else
+                {
+                    e.Graphics.FillRectangle((Brush)Brushes.Red, p.X, p.Y, 1, 2.5f);
+                }
+                
+            }
+            e.Graphics.DrawRectangle(new Pen(Color.FromArgb(255,Color.Black), 3), rect);
+            e.Graphics.DrawEllipse(new Pen(Color.FromArgb(190,Color.White), 3), rect);
+            e.Graphics.FillRectangle((Brush)Brushes.Black, middle.X, middle.Y, 4, 4); //Draws a point at the center of the screen
+
+            if (Points.Count > 0)
+            {
+                float results = (c / Points.Count * 4);
+                resultsTextbox.Text = results.ToString();
+            }
+            
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Points.Clear(); //Clear if any previous points
+            Random rand = new Random();
+            middle.X = panel1.Width / 2;
+            middle.Y = panel1.Height / 2;
+            int x = middle.X - width / 2;
+            int y = middle.Y - height / 2;
+
+            try
+            {
+                int numOfPoints = Int32.Parse(pointsTextbox.Text);
+                for (int i = 0; i < numOfPoints; i++)
+                {
+                    int randX = rand.Next(x, x + width);
+                    int randY = rand.Next(y, y + height);
+                    Points.Add(new Point(randX, randY));
+
+                }
+                panel1.Invalidate(); //Update the panel with the new points
+            }
+            catch(Exception){}
+            
+
+            
+        }
+
+       
+    }
+}
